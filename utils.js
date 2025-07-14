7f27bc677059812c6067f839d7c96c8cdb43af8a
AudaxLudos <james.tarrobal.777@gmail.com>
2025-07-14 22:19:14 +0800

    Ensure prompt container is above other elements

--- utils.js
+++ utils.js
@@ -177,6 +177,7 @@ function getPromptElement() {
     promptContainer.style.left = "0px";
     promptContainer.style.right = "0px";
     promptContainer.style.backdropFilter = "blur(3px)"
+    promptContainer.style.zIndex = "100";
     prompt = document.createElement("div");
     prompt.id = "prompt";
     let gameContent = document.createElement("div");
